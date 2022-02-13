bca4e4647c28c33967a7f3118f84267546085bda
Joshua Maia Rodrigues <joshua.curriculo@gmail.com>
2022-02-12 21:01:19 -0300

    Add tests with Person Service

--- src/test/java/br/com/joshua/baseproject/BaseprojectApplicationTests.java
+++ src/test/java/br/com/joshua/baseproject/BaseprojectApplicationTests.java
@@ -13,28 +13,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.joshua.baseproject.domain.Person;
+import br.com.joshua.baseproject.dto.PersonDto;
 import br.com.joshua.baseproject.repository.PersonRepository;
+import br.com.joshua.baseproject.service.PersonService;
 
 @SpringBootTest
 class BaseprojectApplicationTests {
 
 	@Mock(answer = Answers.RETURNS_SMART_NULLS)
 	PersonRepository repository;
+	
+	@Mock(answer = Answers.RETURNS_SMART_NULLS)
+	PersonService service;
 
 	public Person person = null;
+	public PersonDto personDto = null;
 
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
 
 		person = new Person(1L, "Gina", "gina@gmail.com", LocalDate.now());
+		personDto = new PersonDto(person.getId(), person.getName(), person.getEmail(), person.getBirthDate());
 
 	}
 
@@ -48,18 +54,31 @@ class BaseprojectApplicationTests {
 				return person;
 			}
 		});
+		
+		when(service.save(any(PersonDto.class))).then(new Answer<PersonDto>() {
+
+			@Override
+			public PersonDto answer(InvocationOnMock invocation) throws Throwable {
+				return personDto;
+			}
+		});
 
 		Person response = repository.save(person);
+		PersonDto responseDto = service.save(personDto);
 
 		assertEquals("Gina", response.getName());
 		assertEquals("gina@gmail.com", response.getEmail());
 		assertEquals(1L, response.getId());
+		
+		assertEquals("Gina", responseDto.getName());
+		assertEquals("gina@gmail.com", responseDto.getEmail());
+		assertEquals(1L, responseDto.getId());
 	}
 
 	@Test
 	public void deletePersonTest() {
 		Optional<Person> optionalPerson = Optional.of(person);
-		when(repository.findById(1L)).thenReturn(optionalPerson);
+		when(repository.findById(1L)).thenReturn(optionalPerson);		
 
 		repository.deleteById(1L);
 
@@ -70,15 +89,24 @@ class BaseprojectApplicationTests {
 	public void findOnePersonTest() {
 		Optional<Person> optionalPerson = Optional.of(person);
 		when(repository.findById(1L)).thenReturn(optionalPerson);
+		
+		when(service.findOne(1L)).thenReturn(personDto);
 
 		Optional<Person> personReturnOptional = repository.findById(1L);
 		Person personReturn = personReturnOptional.get();
 		
+		PersonDto personReturnDto = service.findOne(1L);
+		
 		verify(repository, times(1)).findById(1L);
+		verify(service, times(1)).findOne(1L);
 		
 		assertEquals("Gina", personReturn.getName());
 		assertEquals("gina@gmail.com", personReturn.getEmail());
 		assertEquals(1L, personReturn.getId());
+		
+		assertEquals("Gina", personReturnDto.getName());
+		assertEquals("gina@gmail.com", personReturnDto.getEmail());
+		assertEquals(1L, personReturnDto.getId());
 	}
 
 }
