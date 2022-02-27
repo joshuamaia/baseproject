45275e9e12dd0f4863f6e50c7c3591b10e87110b
Joshua Maia Rodrigues <joshua.curriculo@gmail.com>
2022-02-27 20:48:39 -0300

    Fix

--- src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
+++ src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
@@ -3,8 +3,6 @@ package br.com.joshua.baseproject.controller;
 import static org.springframework.http.MediaType.APPLICATION_PDF;
 import static org.springframework.http.ResponseEntity.ok;
 
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joshua.baseproject.service.PersonReportService;
-import net.sf.jasperreports.engine.JRException;
 
 @RestController
 @RequestMapping("/api/persons-reports")
@@ -24,7 +21,7 @@ public class PersonReportController {
 	private PersonReportService service;
 
 	@GetMapping("/pdf")
-	public ResponseEntity<byte[]> generateReportPersons() throws JRException, SQLException {
+	public ResponseEntity<byte[]> generateReportPersons()  {
 
 		byte[] relatorio = service.generatePersonReport();
 		return ok().contentType(APPLICATION_PDF) //
