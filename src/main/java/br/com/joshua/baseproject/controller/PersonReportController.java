b5064adc4716622a113a846bdfef2ad1eabedca4
Joshua Maia Rodrigues <joshua.curriculo@gmail.com>
2022-02-28 20:57:28 -0300

    fix

--- src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
+++ src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
@@ -4,12 +4,9 @@ import static org.springframework.http.MediaType.APPLICATION_PDF;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.http.ResponseEntity.ok;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
