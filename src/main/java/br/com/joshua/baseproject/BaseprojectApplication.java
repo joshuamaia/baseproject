3afe6ae39a1cf23af2890dfb407f5b9fc8d24de0
Joshua Maia Rodrigues <joshua.curriculo@gmail.com>
2022-03-19 20:04:17 -0300

    Title Swagger changed

--- src/main/java/br/com/joshua/baseproject/BaseprojectApplication.java
+++ src/main/java/br/com/joshua/baseproject/BaseprojectApplication.java
@@ -3,7 +3,11 @@ package br.com.joshua.baseproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Base Project API", version = "1.0", description = "Base Project Information"))
 public class BaseprojectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
+++ src/main/java/br/com/joshua/baseproject/controller/PersonReportController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.joshua.baseproject.dto.ExpenseControlDto;
 import br.com.joshua.baseproject.service.PersonReportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
